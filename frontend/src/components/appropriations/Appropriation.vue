8efdd4db1641fc66999242768bd4143ea78016db
iamfrank <frank@magenta.dk>
2019-05-10 15:43:44 +0200

    stuff

--- frontend/src/components/appropriations/Appropriation.vue
+++ frontend/src/components/appropriations/Appropriation.vue
@@ -2,21 +2,52 @@
 
     <section class="appropriation" v-if="appr">
         <router-link to="/case/1">Sag XXX</router-link>
-        <h1>Bevillingsskrivelse {{ appr.sbsys_id }}</h1>
-        <dl>
-            <dt>Paragraf</dt>
-            <dd>{{ appr.law_ref }}</dd>
-            <dt>Status</dt>
-            <dd><span :class="`status-${ appr.status }`">{{ appr.status }}</span></dd>
-            <template v-if="appr.approval">
-                <dt>Bevilget af</dt>
-                <dd>
-                    {{ appr.approval.approval_person }}, 
-                    {{ appr.approval.approval_auth_level }} 
-                    - {{ new Date(appr.approval.approval_date).toLocaleDateString() }}
-                </dd>
-            </template>
-        </dl>
+        <h1>Bevillingsskrivelse</h1>
+        <div style="display: grid; grid-template-columns: 1fr 1fr 1fr;">
+            <div style="margin: 1rem;">
+                <dl>
+                    <dt>SBSYS-sag</dt>
+                    <dd><strong>{{ appr.sbsys_id}}</strong></dd>
+                    <dt>Bevilling efter §</dt>
+                    <dd>{{ appr.law_ref }}</dd>
+                    <dt>Status</dt>
+                    <dd>
+                        <span :class="`status-${ appr.status }`">{{ appr.status }}</span>
+                        <template v-if="appr.approval">
+                            af
+                            {{ appr.approval.approval_person }}, 
+                            {{ appr.approval.approval_auth_level }} 
+                            - {{ new Date(appr.approval.approval_date).toLocaleDateString() }}
+                        </template>
+                    </dd>
+                    
+                </dl>
+            </div>
+            <div style="margin: 1rem;">
+                <h2>Sagspart</h2>
+                <dl>
+                    <dt>CPR-nr</dt>
+                    <dd>{{ appr.case.cpr_no }}</dd>
+                    <dt>Navn</dt>
+                    <dd>{{ appr.case.name }}</dd>
+                    <dt>Hovedsag</dt>
+                    <dd>{{ appr.case.sbsys_id}}</dd>
+                    <dt>Sagsbehandler</dt>
+                    <dd>{{ appr.case.case_worker}}</dd>
+                </dl>
+            </div>
+            <div style="margin: 1rem;">
+                <h2>Kommune</h2>
+                <dl>
+                    <dt>Betalingskommune</dt>
+                    <dd>{{ appr.case.municipality_payment}}</dd>
+                    <dt>Handlekommune</dt>
+                    <dd>{{ appr.case.municipality_action}}</dd>
+                    <dt>Bopælskommune</dt>
+                    <dd>{{ appr.case.municipality_residence}}</dd>
+                </dl>
+            </div>
+        </div>
         <activity-list :appr-id="appr.pk" />
     </section>
 
@@ -64,7 +95,7 @@
     }
 
     .appropriation .status-bevilget {
-        background-color: green;
+        background-color: var(--success);
         color: white;
         padding: .25rem;
     }
