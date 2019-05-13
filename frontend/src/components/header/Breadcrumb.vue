e12c298288566b5dc91f73777bcb23bc50f64992
iamfrank <frank@magenta.dk>
2019-05-13 15:42:20 +0200

    adding some navigation features

---
+++ frontend/src/components/header/Breadcrumb.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <nav class="breadcrumb">
+        <span v-for="b in bc" :key="b[0]" class="crumb">
+            <router-link :to="b.link">
+                {{ b.title }}
+            </router-link>
+        </span>
+    </nav>
+
+</template>
+
+<script>
+
+    export default {
+
+        computed: {
+            bc: function() {
+                return this.$store.getters.getBreadcrumb
+            }
+        }
+        
+    }
+    
+</script>
+
+<style>
+
+    .breadcrumb {
+        font-size: 0.85em;
+        padding: .25rem .5rem;
+        border-bottom: solid 1px var(--grey1);
+    }
+
+    .breadcrumb .crumb {
+        display: inline-block;
+    }
+
+    .breadcrumb .crumb::before {
+        content: '>';
+        display: inline-block;
+        margin: .5rem;
+        opacity: .5;
+    }
+
+</style>
--- frontend/src/components/header/Header.vue
+++ frontend/src/components/header/Header.vue
@@ -1,15 +1,24 @@
 <template>
 
-    <header class="globalheader">
-        <router-link to="/">Mine sager</router-link>
-        <router-link to="/login">Log ud</router-link>
-    </header>
+    <header>
+        <nav class="globalheader">
+            <router-link to="/">Mine sager</router-link>
+            <router-link to="/login">Log ud</router-link>
+        </nav>
+        <breadcrumb />
+    </header> 
 
 </template>
 
 <script>
+
+    import Breadcrumb from './Breadcrumb.vue'
     
     export default {
+
+        components: {
+            Breadcrumb
+        }
 
     }
 
