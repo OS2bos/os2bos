3420a7ec6c469427031db085dbcae87d9e532ebd
Søren Howe Gersager <thesyre@pm.me>
2019-05-15 16:03:41 +0200

    add docstrings based on carstens feedback

--- backend/bevillingsplatform/initialize.py
+++ backend/bevillingsplatform/initialize.py
@@ -5,9 +5,14 @@ from core.models import Municipality
 
 
 def initialize():
+    """Main script for initializing all the basic data we want at start.
+
+    Should be able to be run multiple times over without generating duplicates.
+    """
     initialize_municipalities()
 
 
 def initialize_municipalities():
+    """Initialize all the danish municipalities."""
     for name in municipalities:
         Municipality.objects.get_or_create(name=name)
