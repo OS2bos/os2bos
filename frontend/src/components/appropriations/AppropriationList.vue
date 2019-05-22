a0de0b5685292202b50ed3e40ea5d860a6be9fcf
iamfrank <frank@magenta.dk>
2019-05-22 11:54:20 +0200

    Change labels and layout

--- frontend/src/components/appropriations/AppropriationList.vue
+++ frontend/src/components/appropriations/AppropriationList.vue
@@ -2,19 +2,21 @@
 
     <section class="appropriations">
         <header class="appropriations-header">
-            <h1>Bevillinger</h1>
-            <button class="appropriation-create-btn" @click="createAppr()">+ Ny bevilling</button>
+            <h1>Foranstaltninger</h1>
+            <button class="appropriation-create-btn" @click="createAppr()">+ Tilføj foranstaltning</button>
         </header>
         <table>
             <thead>
-            <tr>
-                <th>Sags-ID</th>
-                <th>Foranstaltningsudgift</th>
-                <th>Aktivitet</th>
-                <th>Følgeudgift</th>
-                <th>Økonomi</th>
-                <th>Status</th>
-            </tr>
+                <tr>
+                    <th>Foranstaltningssag</th>
+                    <th>Foranstaltningsudgift</th>
+                    <th>Aktivitet</th>
+                    <th>Følgeudgift</th>
+                    <th>Status</th>
+                    <th>Oprettet</th>
+                    <th>Senest ændret</th>
+                    <th style="text-align: right">Økonomi</th>
+                </tr>
             </thead>
             <tbody>
                 <tr v-for="a in apprs" :key="a[0]">
@@ -26,8 +28,21 @@
                     <td>{{ a.activities.main_law_ref }}</td>
                     <td>{{ a.activities.main_activity_name }}</td>
                     <td>{{ a.activities.activities_count }}</td>
-                    <td>{{ a.payment.total_amount }}</td>
+                    
                     <td><span class="status">{{ a.status }}</span></td>
+                    <td>{{ new Date(a.created_date).toLocaleDateString() }}</td>
+                    <td>{{ new Date(a.modified_date).toLocaleDateString() }}</td>
+                    <td style="text-align: right">{{ a.payment.total_amount }} kr</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td style="text-align: right">Samlet</td>
+                    <td style="text-align: right"><strong>{{ total_amounts }}</strong> kr</td>
                 </tr>
             </tbody>
         </table>
@@ -47,6 +62,18 @@
         data: function() {
             return {
                 apprs: null   
+            }
+        },
+        computed: {
+            total_amounts: function() {
+                function getTotal(total, a) {
+                    console.log(total)
+                    console.log(a.payment.total_amount)
+                    return total + a.payment.total_amount
+                }
+                if (this.apprs) {
+                    return this.apprs.reduce(getTotal, 0)
+                }
             }
         },
         methods: {
