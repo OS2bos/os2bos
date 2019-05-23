20f0b13aedd39b093290d4c3fab442aa7fea37fd
iamfrank <frank@magenta.dk>
2019-05-23 13:56:35 +0200

    hook up some api

--- frontend/src/components/cases/Cases.vue
+++ frontend/src/components/cases/Cases.vue
@@ -20,15 +20,15 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="c in cas" :key="c.pk">
+                <tr v-for="c in cas" :key="c.id">
                     <td>
                         <i class="material-icons">folder_shared</i>
-                        <router-link :to="`/case/${ c.pk }`">
+                        <router-link :to="`/case/${ c.id }`">
                             {{ c.sbsys_id }}
                         </router-link>
                     </td>
                     <td>
-                        {{ c.cpr_no }} - {{ c.name }}
+                        {{ c.cpr_number }} - {{ c.name }}
                     </td>
                     <td>
                         {{ displayDate(c.modified) }}
@@ -57,8 +57,9 @@
                 this.fetchCases(this.$route.params.id)
             },
             fetchCases: function(id) {
-                axios.get('../../case-data.json')
+                axios.get('/cases/')
                 .then(res => {
+                    console.log('CASES!')
                     this.cas = res.data
                 })
                 .catch(err => console.log(err))
--- frontend/src/components/footer/Footer.vue
+++ frontend/src/components/footer/Footer.vue
@@ -2,13 +2,84 @@
 
     <footer class="globalfooter">
         
+        <button @click="submitTestData()">Opfind data (kun test)</button>
+
     </footer>
 
 </template>
 
 <script>
+
+    import axios from '../http/Http.js'
     
     export default {
+
+        methods: {
+            submitTestData: function() {
+
+                axios.post('/cases/', {
+                    "user_created": "leil",
+                    "sbsys_id": "46.46.24-64-ghis-872",
+                    "cpr_number": "000000-0000",
+                    "name": "Ejvind-Emil Hansen Persdatter",
+                    "case_worker": "Inger Støjberg",
+                    "target_group": "FAMILY_DEPT",
+                    "refugee_integration": true,
+                    "cross_department_measure": true,
+                    "district": "1",
+                    "paying_municipality": "1",
+                    "acting_municipality": "1",
+                    "residence_municipality": "1"
+                })
+                .then(res => {
+                    console.log('posting went well')   
+                })
+                .catch(err => {
+                    console.log('posting was a failure')   
+                })
+                axios.post('/cases/', {
+                    "user_created": "Inger Støjberg",
+                    "sbsys_id": "63.14.75-17-XHhi-4",
+                    "cpr_number": "000000-0000",
+                    "name": "Mona Smith",
+                    "case_worker": "Inger Støjberg",
+                    "target_group": "DISABILITY_DEPT",
+                    "refugee_integration": false,
+                    "cross_department_measure": false,
+                    "district": "2",
+                    "paying_municipality": "2",
+                    "acting_municipality": "1",
+                    "residence_municipality": "1"
+                })
+                .then(res => {
+                    console.log('posting went well')   
+                })
+                .catch(err => {
+                    console.log('posting was a failure')   
+                })
+                axios.post('/cases/', {
+                    "user_created": "leil",
+                    "sbsys_id": "56.17.45-10-bnih-84",
+                    "cpr_number": "000000-0000",
+                    "name": "Génevieve Macron",
+                    "case_worker": "Leif L Lodahl",
+                    "target_group": "FAMILY_DEPT",
+                    "refugee_integration": false,
+                    "cross_department_measure": false,
+                    "district": "2",
+                    "paying_municipality": "1",
+                    "acting_municipality": "1",
+                    "residence_municipality": "1"
+                })
+                .then(res => {
+                    console.log('posting went well')   
+                })
+                .catch(err => {
+                    console.log('posting was a failure')   
+                })
+
+            }
+        }
 
     }
 
