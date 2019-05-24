a1be69e5261fc4062e05bd7b0616dc18dfe938c9
iamfrank <frank@magenta.dk>
2019-05-24 15:28:46 +0200

    sflih

--- frontend/src/components/cases/CaseEdit.vue
+++ frontend/src/components/cases/CaseEdit.vue
@@ -4,109 +4,114 @@
             <h1>Tilknyt/Redigér hovedsag</h1>
             
             <fieldset>
-                <label for="inputSearch">SBSYS Hovedsag:</label>
-                <input id="inputSearch" type="search" :value="cas.sbsys_id">
-                <button class="case-button">Hent</button>
+                <label for="field-sbsys-id">SBSYS Hovedsag:</label>
+                <input id="field-sbsys-id" type="search" v-model="cas.sbsys_id">
+                <button class="case-button" disabled>Hent</button>
             </fieldset>
         
-            <div>
+            <fieldset>
+                <label for="field-name">Sagspart, navn</label>
+                <input id="field-name" type="text" v-model="cas.name">
+            </fieldset>
 
-                <fieldset>
-                    <label for="field-name">Sagspart, navn</label>
-                    <input id="field-name" type="text" v-model="cas.name">
-                </fieldset>
-                <fieldset>
-                    <label for="field-cpr">Sagspart, CPR-nr</label>
-                    <input id="field-cpr" type="text" v-model="cas.cpr_number">
-                </fieldset>
+            <fieldset>
+                <label for="field-cpr">Sagspart, CPR-nr</label>
+                <input id="field-cpr" type="text" v-model="cas.cpr_number">
+            </fieldset>
 
-                <fieldset>
-                    <h3>Familie og relationer:</h3>
-                    <dl>
-                        <dt>Mor:</dt>
-                        <dd>ikke implementeret</dd>
-                        <dt>Far:</dt>
-                        <dd>ikke implementeret</dd>
-                        <dt>Andre:</dt>
-                        <dd>ikke implementeret</dd>
-                    </dl>
-                </fieldset>
+            <fieldset>
+                <h3>Familie og relationer:</h3>
+                <dl>
+                    <dt>Mor:</dt>
+                    <dd>ikke implementeret</dd>
+                    <dt>Far:</dt>
+                    <dd>ikke implementeret</dd>
+                    <dt>Andre:</dt>
+                    <dd>ikke implementeret</dd>
+                </dl>
+            </fieldset>
 
-                <fieldset>
-                    <h3>Kommune:</h3>
-                    <label for="selectField1">Betalingskommune:</label>
-                    <select id="selectField1">
-                        <option>{{ cas.paying_municipality }}</option>
-                    </select>
-                </fieldset>
-                <fieldset>
-                    <label for="selectField2">Handlekommune:</label>
-                    <select id="selectField2">
-                        <option>{{ cas.acting_municipality }}</option>
-                    </select>
-                </fieldset>
-                <fieldset>
-                    <label for="selectField3">Bopælsskommune:</label>
-                    <select id="selectField3">
-                        <option>{{ cas.residence_municipality }}</option>
-                    </select>
-                </fieldset>
+            <fieldset>
+                <h3>Kommune:</h3>
+                <label for="selectField1">Betalingskommune:</label>
+                <select id="selectField1">
+                    <option>{{ cas.paying_municipality }}</option>
+                </select>
+            </fieldset>
 
-                <fieldset>
-                    <h3>Målgruppe:</h3>
-                    <input id="inputRadio1" type="radio" name="radioButtonSet">
-                    <label for="inputRadio1">Familieafdelingen</label>
-                    <input id="inputRadio2" type="radio" name="radioButtonSet">
-                    <label for="inputRadio2">Handicapafdelingen</label>
-                </fieldset>
+            <fieldset>
+                <label for="selectField2">Handlekommune:</label>
+                <select id="selectField2">
+                    <option>{{ cas.acting_municipality }}</option>
+                </select>
+            </fieldset>
 
-                <fieldset>
-                    <h3>Andet:</h3>
-                    <input id="inputCheckbox1" type="checkbox" name="radioButtonSet">
-                    <label for="inputCheckbox1">Integrationsindsatsen</label>
-                    <input id="inputCheckbox2" type="checkbox" name="radioButtonSet">
-                    <label for="inputCheckbox2">Tværgående ungeindsats</label>
-                </fieldset>
-                <fieldset>
-                    <label for="selectField">Oprindeligt distrikt:</label>
-                    <select id="selectField">
-                        <option>{{ cas.original_district}}</option>
-                    </select>
-                </fieldset>
-                <fieldset>
-                    <dt>Indsatstrappen:</dt>
-                    <dd>{{ cas.effort_stairs }}</dd>
-                    <dt>Skaleringstrappe:</dt>
-                    <dd>{{ cas.scaling_staircase }}</dd>
-                </fieldset>
-                <fieldset>
-                    <button @click="$router.push(`/assessment/${ cas.pk }`)">Vurdering</button>
-                </fieldset>
+            <fieldset>
+                <label for="selectField3">Bopælsskommune:</label>
+                <select id="selectField3">
+                    <option>{{ cas.residence_municipality }}</option>
+                </select>
+            </fieldset>
 
+            <fieldset>
+                <h3>Målgruppe:</h3>
+                <input id="inputRadio1" type="radio" name="radioButtonSet">
+                <label for="inputRadio1">Familieafdelingen</label>
+                <input id="inputRadio2" type="radio" name="radioButtonSet">
+                <label for="inputRadio2">Handicapafdelingen</label>
+            </fieldset>
+
+            <fieldset>
+                <h3>Andet:</h3>
+                <input id="inputCheckbox1" type="checkbox" name="radioButtonSet">
+                <label for="inputCheckbox1">Integrationsindsatsen</label>
+                <input id="inputCheckbox2" type="checkbox" name="radioButtonSet">
+                <label for="inputCheckbox2">Tværgående ungeindsats</label>
+            </fieldset>
+
+            <fieldset>
+                <label for="selectField">Oprindeligt distrikt:</label>
+                <select id="selectField">
+                    <option>{{ cas.original_district}}</option>
+                </select>
+            </fieldset>
+
+            <fieldset>
+                <dt>Indsatstrappen:</dt>
+                <dd>{{ cas.effort_stairs }}</dd>
+                <dt>Skaleringstrappe:</dt>
+                <dd>{{ cas.scaling_staircase }}</dd>
+            </fieldset>
+
+            <fieldset>
+                <button @click="$router.push(`/assessment/${ cas.pk }`)">Vurdering</button>
+            </fieldset>
+
+            <fieldset>
+                <h3>Sagsbehandling:</h3>
+                <dl>
+                    <dt>Sagsbehander:</dt>
+                    <dd>{{ cas.case_worker }}</dd>
+                    <dt>Team:</dt>
+                    <dd>ikke implementeret</dd>
+                </dl>
                 <fieldset>
-                    <h3>Sagsbehandling:</h3>
-                    <dl>
-                        <dt>Sagsbehander:</dt>
-                        <dd>{{ cas.case_worker }}</dd>
-                        <dt>Team:</dt>
-                        <dd>ikke implementeret</dd>
-                    </dl>
-                    <fieldset>
-                    <label for="selectField">Distrikt:</label>
-                    <select id="selectField">
-                        <option>{{ cas.district }}</option>
-                    </select>
-                    </fieldset>
-                    <fieldset>
-                        <dt>Leder:</dt>
-                        <dd>ikke implementeret</dd>
-                    </fieldset>
+                <label for="selectField">Distrikt:</label>
+                <select id="selectField">
+                    <option>{{ cas.district }}</option>
+                </select>
                 </fieldset>
                 <fieldset>
-                    <input type="submit" value="Gem">
-                    <button type="cancel">Annullér</button>
+                    <dt>Leder:</dt>
+                    <dd>ikke implementeret</dd>
                 </fieldset>
-            </div>        
+            </fieldset>
+            
+            <fieldset>
+                <input type="submit" value="Gem">
+                <button type="cancel">Annullér</button>
+            </fieldset>
+            
         </form>
     </section>
 
