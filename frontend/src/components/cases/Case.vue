e6918b480d137e5842437a239fa9e34cefa6389d
iamfrank <frank@magenta.dk>
2019-05-24 14:44:05 +0200

    stuff

--- frontend/src/components/cases/Case.vue
+++ frontend/src/components/cases/Case.vue
@@ -1,42 +1,61 @@
 <template>
 
     <section class="case" v-if="cas">
+
         <header class="case-header">
             <h1 style="padding: 0;">
                 <i class="material-icons">folder_shared</i>
                 Hovedsag {{ cas.sbsys_id }}
             </h1>
-            <button>Redigér</button>
+            <button @click="edit_mode = !edit_mode">Redigér</button>
         </header>
-        <div class="case-info">
-        <dl style="flex: 1 0 33%;">
-            <dt>Sagspart (CPR, navn)</dt>
-            <dd>{{ cas.cpr_number }}, {{ cas.name }}</dd>
-            <dt>Indsatstrappen:</dt>
-            <dd>ikke implementeret</dd>
-            <dt>Skaleringstrappe:</dt>
-            <dd>ikke implementeret</dd>
-        </dl>
-        <dl style="flex: 1 0 33%;">
-            <dt>Sagsbehander:</dt>
-            <dd>{{ cas.case_worker }}</dd>
-            <dt>Team:</dt>
-            <dd>ikke implementeret</dd>
-            <dt>Distrikt:</dt>
-            <dd>{{ cas.district}}</dd>
-            <dt>Leder:</dt>
-            <dd>ikke implementeret</dd>
-        </dl>
-        <dl style="flex: 1 0 33%;">
-            <dt>Betalingskommune:</dt>
-            <dd>{{ cas.paying_municipality }}</dd>
-            <dt>Handlekommune:</dt>
-            <dd>{{ cas.acting_municipality }}</dd>
-            <dt>Bopælsskommune:</dt>
-            <dd>{{ cas.residence_municipality }}</dd>
-        </dl>
+
+        <div class="case-info" v-if="!edit_mode">
+            <dl style="flex: 1 0 33%;">
+                <dt>Sagspart (CPR, navn)</dt>
+                <dd>{{ cas.cpr_number }}, {{ cas.name }}</dd>
+                <dt>Indsatstrappen:</dt>
+                <dd>ikke implementeret</dd>
+                <dt>Skaleringstrappe:</dt>
+                <dd>ikke implementeret</dd>
+            </dl>
+            <dl style="flex: 1 0 33%;">
+                <dt>Sagsbehander:</dt>
+                <dd>{{ cas.case_worker }}</dd>
+                <dt>Team:</dt>
+                <dd>ikke implementeret</dd>
+                <dt>Distrikt:</dt>
+                <dd>{{ cas.district}}</dd>
+                <dt>Leder:</dt>
+                <dd>ikke implementeret</dd>
+            </dl>
+            <dl style="flex: 1 0 33%;">
+                <dt>Betalingskommune:</dt>
+                <dd>{{ cas.paying_municipality }}</dd>
+                <dt>Handlekommune:</dt>
+                <dd>{{ cas.acting_municipality }}</dd>
+                <dt>Bopælsskommune:</dt>
+                <dd>{{ cas.residence_municipality }}</dd>
+            </dl>
         </div>
+
+        <form v-if="edit_mode" @submit.prevent="saveChanges()">
+            <fieldset>
+                <label for="field-name">Sagspart, navn</label>
+                <input id="field-name" type="text" v-model="cas.name">
+            </fieldset>
+            <fieldset>
+                <label for="field-cpr">Sagspart, CPR-nr</label>
+                <input id="field-cpr" type="text" v-model="cas.cpr_number">
+            </fieldset>
+            <fieldset>
+                <input type="submit" value="Gem">
+                <button type="cancel">Annullér</button>
+            </fieldset>
+        </form>
+
         <appropriations :case-id="cas.id" />
+
     </section>
 
 </template>
@@ -54,7 +73,8 @@
         
         data: function() {
             return {
-                cas: null
+                cas: null,
+                edit_mode: false
             }
         },
         methods: {
@@ -75,6 +95,17 @@
                             title: `${ this.cas.sbsys_id}, ${ this.cas.name}`
                         }
                     ])
+                })
+                .catch(err => console.log(err))
+            },
+            saveChanges: function() {
+                axios.patch(`/cases/${ this.cas.id }/`, {
+                    name: this.cas.name,
+                    cpr_number: this.cas.cpr_number
+                })
+                .then(() => {
+                    this.edit_mode =  false
+                    this.update()
                 })
                 .catch(err => console.log(err))
             }
