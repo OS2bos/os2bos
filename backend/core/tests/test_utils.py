d090805dbc4cb1b3d0232f4bf6e792200f8c8750
Søren Howe Gersager <thesyre@pm.me>
2019-05-27 10:40:03 +0200

    add test succesful case

--- backend/core/tests/test_utils.py
+++ backend/core/tests/test_utils.py
@@ -24,3 +24,17 @@ class GetCPRDataTestCase(TestCase):
             self.assertIsNone(result)
             self.assertEqual(len(log.output), 1)
             self.assertIn("get_cpr_data requests error", log.output[0])
+
+    @mock.patch("core.utils.os.path.isfile")
+    @mock.patch("core.utils.get_citizen")
+    def test_get_cpr_data_success(self, get_citizen_mock, mock_isfile):
+        mock_isfile.return_value = True
+        result_data = {
+            "fornavn": "Jens Jensner",
+            "efternavn": "Jensen",
+            "relationer": [{"cprnr": "0123456780", "relation": "aegtefaelle"}],
+        }
+        get_citizen_mock.return_value = result_data
+        result = get_cpr_data("1234567890")
+
+        self.assertEqual(result["fornavn"], "Jens Jensner")
