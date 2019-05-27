02535aeecf377552bca28f5feb8305d8f2dd1299
Søren Howe Gersager <thesyre@pm.me>
2019-05-27 11:46:34 +0200

    setup local postfix, load EMAIL settings from ini

--- backend/bevillingsplatform/settings.py
+++ backend/bevillingsplatform/settings.py
@@ -150,3 +150,11 @@ REST_FRAMEWORK = {
         "django_filters.rest_framework.DjangoFilterBackend",
     )
 }
+
+# Email settings
+EMAIL_BACKEND = default_config.get("EMAIL_BACKEND")
+EMAIL_HOST_USER = default_config.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = default_config.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = default_config.get("EMAIL_HOST")
+EMAIL_PORT = default_config.get("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = default_config.get("DEFAULT_FROM_EMAIL")
