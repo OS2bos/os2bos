14517a7e294fd5a69cb4eae6775d44f2ee36d4e8
Søren Howe Gersager <thesyre@pm.me>
2019-06-04 13:43:04 +0200

    add testing mixins

---
+++ backend/core/tests/testing_mixins.py
@@ -0,0 +1,22 @@
+from core.models import Case, Municipality, SchoolDistrict
+
+
+class CaseMixin:
+
+    @staticmethod
+    def create_case():
+        municipality = Municipality.objects.create(name="København")
+        district = SchoolDistrict.objects.create(name="Baltorpskolen")
+        case = Case.objects.create(
+            sbsys_id="13212",
+            cpr_number="0205891234",
+            name="Jens Jensen",
+            case_worker="Orla Frøsnapper",
+            scaling_step=1,
+            effort_step="STEP_ONE",
+            acting_municipality_id=municipality.id,
+            district_id=district.id,
+            paying_municipality_id=municipality.id,
+            residence_municipality_id=municipality.id,
+        )
+        return case
