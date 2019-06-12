a2295c8eccae331dcad0d2e6855675e29b9fb504
Ubuntu <carstena@magenta-aps.dk>
2019-06-12 13:33:22 +0000

    Allow session-based logins so tests will pass.

--- backend/bevillingsplatform/urls.py
+++ backend/bevillingsplatform/urls.py
@@ -38,6 +38,7 @@ router.register(r"users", views.UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("rest_framework.urls")),
     path("token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
     path("", include(router.urls)),
