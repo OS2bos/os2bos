96068d57532cff8578666fbb63187bcc9bb1cc50
Mathias Dannesbo <md@magenta.dk>
2019-06-12 11:17:29 +0200

    Wait for database

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-sleep 5
+trap exit 1 SIGINT
+
+MAX=30
+for (( c=1; c<=$MAX; c++ )); do
+    a=`printf %02d/%02d $c $MAX`
+    if ./manage.py inspectdb > /dev/null 2>&1; then
+        printf "$a Connected to database."
+        break
+    elif [ $c -ge $MAX ]; then
+        echo "$a Unable to connect to database. Giving up."
+        exit 1
+    fi
+    echo "$a Unable to connect to database."
+    sleep 1;
+done;
 
 
 #./manage.py flush --no-input
