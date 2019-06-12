5c83c3edca9c223e4cbdd4440beb2a484d0a893e
Ubuntu <carstena@magenta-aps.dk>
2019-06-12 13:50:26 +0000

    YALE.

--- backend/bevillingsplatform/urls.py
+++ backend/bevillingsplatform/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from rest_framework import routers
-from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from core import views
 
@@ -39,7 +42,7 @@ router.register(r"users", views.UserViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("rest_framework.urls")),
-    path("token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path("token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", include(router.urls)),
 ] + static("/static/", document_root=settings.STATIC_ROOT)
