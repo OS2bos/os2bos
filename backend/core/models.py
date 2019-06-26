31303b1eccc6f5366fb980c0a9ac54700a224b27
Carsten Agger <carstena@magenta.dk>
2019-06-26 13:14:14 +0000

    Updated appropriation PDF a bit more.

--- backend/core/models.py
+++ backend/core/models.py
@@ -421,7 +421,7 @@ class Activity(AuditModelMixin, models.Model):
         Appropriation,
         null=True,
         blank=True,
-        related_name="activities",
+        related_name="main_activity",
         on_delete=models.CASCADE,
     )
 
