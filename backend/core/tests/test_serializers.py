79f36276093b75c4657250440f90823f6e4366dd
Søren Howe Gersager <thesyre@pm.me>
2019-06-28 14:24:27 +0200

    add validate for ActivitySerializer start < end

--- backend/core/tests/test_serializers.py
+++ backend/core/tests/test_serializers.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from core.models import ActivityCatalog, Activity
 from core.tests.testing_mixins import ActivityMixin, PaymentScheduleMixin
 from core.serializers import ActivitySerializer
 
@@ -17,3 +18,38 @@ class ActivitySerializerTestCase(
         )
         data = ActivitySerializer(activity).data
         self.assertEqual(data["total_amount"], activity.total_amount())
+
+    def test_validate_end_before_start(self):
+        activity_catalog = ActivityCatalog.objects.create(
+            max_tolerance_in_percent=10, max_tolerance_in_dkk=1000
+        )
+        # start_date > end_date
+        data = {
+            "start_date": "2019-01-01",
+            "end_date": "2018-01-01",
+            "service": activity_catalog.pk,
+            "status": Activity.STATUS_EXPECTED,
+            "activity_type": Activity.MAIN_ACTIVITY
+        }
+        serializer = ActivitySerializer(data=data)
+        serializer.is_valid()
+        self.assertEqual(
+            serializer.errors["non_field_errors"][0],
+            "startdato skal være før slutdato"
+        )
+
+    def test_validate_start_before_end(self):
+        activity_catalog = ActivityCatalog.objects.create(
+            max_tolerance_in_percent=10, max_tolerance_in_dkk=1000
+        )
+        # start_date < end_date
+        data = {
+            "start_date": "2018-01-01",
+            "end_date": "2019-01-01",
+            "service": activity_catalog.pk,
+            "status": Activity.STATUS_EXPECTED,
+            "activity_type": Activity.MAIN_ACTIVITY
+        }
+        serializer = ActivitySerializer(data=data)
+        serializer.is_valid()
+        self.assertEqual(serializer.errors, {})
