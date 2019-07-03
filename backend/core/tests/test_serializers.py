448e36c1c045d1266f5cc5bb6b64366403820cc2
Søren Howe Gersager <thesyre@pm.me>
2019-07-03 11:49:10 +0200

    remove test from ActivitySerializer too

--- backend/core/tests/test_serializers.py
+++ backend/core/tests/test_serializers.py
@@ -12,16 +12,6 @@ from core.serializers import ActivitySerializer, CaseSerializer
 class ActivitySerializerTestCase(
     TestCase, ActivityMixin, PaymentScheduleMixin
 ):
-    def test_get_total_amount(self):
-        activity = self.create_activity()
-        payment_schedule = self.create_payment_schedule()
-        activity.payment_plan = payment_schedule
-        activity.save()
-        payment_schedule.generate_payments(
-            activity.start_date, activity.end_date
-        )
-        data = ActivitySerializer(activity).data
-        self.assertEqual(data["total_amount"], activity.total_amount())
 
     def test_validate_end_before_start(self):
         activity_details = ActivityDetails.objects.create(
