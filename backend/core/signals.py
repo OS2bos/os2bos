20652c5a9bd9453d66630cfb73f45e0861dd916a
Søren Howe Gersager <thesyre@pm.me>
2019-07-05 11:11:02 +0200

    import signals in CoreConfig

---
+++ backend/core/signals.py
@@ -0,0 +1,27 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from core.models import Activity, Appropriation
+from core.utils import send_activity_created_email, send_activity_changed_email
+
+
+@receiver(post_save, sender=Activity)
+def send_email_on_save(sender, instance, created, **kwargs):
+    # If activity or appropriation is not granted we don't send an email.
+    if (
+        not instance.status == Activity.STATUS_GRANTED
+        or not instance.appropriation.status == Appropriation.STATUS_GRANTED
+    ):
+        return
+
+    # Don't trigger the email if the payment plan does not exist
+    # or does not need a payment email to trigger.
+    if not hasattr(instance, "payment_plan"):
+        return
+    if not instance.payment_plan.triggers_payment_email():
+        return
+
+    if created:
+        send_activity_created_email(instance)
+    else:
+        send_activity_changed_email(instance)
