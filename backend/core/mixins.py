3967d6a48e7fd9e4ed9b015e2206c933b88f2050
Carsten Agger <carstena@magenta.dk>
2019-07-16 10:43:15 +0000

    Make tests pass.

--- backend/core/mixins.py
+++ backend/core/mixins.py
@@ -9,7 +9,7 @@ class AuditMixin:
     # By default log all calls that change data. Change e.g. by adding
     # "get" as needed.
     log_methods = ("post", "put", "patch", "delete")
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("bevillingsplatform.audit")
 
     def finalize_response(self, request, response, *args, **kwargs):
         """Perform logging and continue normal operations."""
@@ -17,10 +17,15 @@ class AuditMixin:
         response = super().finalize_response(
             request, response, *args, **kwargs
         )
+        status_code = response.status_code
 
         if request.method.lower() in self.log_methods:
             # Now perform logging.
-            if response.is_error():
-                logger.error(f"ERROR: {response.text}")
+            if status.is_server_error(status_code):
+                self.logger.error(f"SERVER ERROR: {response.data}")
+            if status.is_client_error(status_code):
+                self.logger.error(f"CLIENT ERROR: {response.data}")
             else:
-                logger.info(f"INFO: {response.text}")
+                self.logger.info(f"INFO: {response.data}")
+
+        return response
