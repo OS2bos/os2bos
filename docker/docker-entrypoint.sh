681a1f74b73e1ec48dcbb29ec12a508e3ed6ed6e
Steffen Park <dev@istherepie.com>
2019-08-21 13:52:41 +0200

    30087: Migrations are committed and no longer need to be generated at startup.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -11,19 +11,10 @@ set -ex
 
 ./manage.py ensure_db_connection --wait 30
 
-# TODO: As the migrations are not committed during the development phase, we
-# make the migrations on runtime in docker-entrypoint.sh. To do this we chown
-# the correct folder in Dockerfile. When the model have stabilized, the
-# migrations should be comitted to the repository and these lines should be
-# removed. See https://redmine.magenta-aps.dk/issues/30087
-./manage.py makemigrations
-
+# Run Migrate
 ./manage.py migrate
 
-# Seed database
-# Reference: `bevillingsplatform.initialize`
-./manage.py initialize_database
-
+# Generate static content
 ./manage.py collectstatic --no-input --clear
 
 exec "$@"
