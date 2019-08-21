dfe77473f361fced3b5ad939f7a378bb43973d86
Novik <ns@magenta.dk>
2019-08-21 11:42:23 +0200

    Removed component assessment edit

--- frontend/src/components/assessments/Assessment.vue
+++ frontend/src/components/assessments/Assessment.vue
@@ -7,24 +7,10 @@
 
 
 <template>
-    <div class="assessment" v-if="cas">
+    <div class="assessment_history" v-if="cas">
         <header class="assessment-header">
             <h1>Vurderinger af {{ cas.cpr_number }}, {{ cas.name }}</h1>
         </header>
-
-        <form class="assessment-form" @submit.prevent="saveAssessment()">
-            <div class="row">
-                <div class="column">
-                    <assessment-edit :case-obj="cas" @assessment="updateAssessment" />
-                    <fieldset style="margin: 0 1rem 1rem;">
-                        <input :disabled="disableButton" type="submit" value="Opdatér">
-                        <button class="cancel-btn" type="button" @click="cancel()">Annullér</button>
-                    </fieldset>
-
-                </div>
-                <div class="column"></div>
-            </div>
-        </form>
 
         <assessment-history :case-obj="cas" />
     </div>
@@ -33,15 +19,13 @@
 
 <script>
 
-    import AssessmentEdit from './AssessmentEdit.vue'
     import AssessmentHistory from './AssessmentHistory.vue'
     import axios from '../http/Http.js'
 
     export default {
 
         components: {
-            AssessmentHistory,
-            AssessmentEdit
+            AssessmentHistory
         },
 
         data: function() {
@@ -76,34 +60,6 @@
                 })
                 .catch(err => console.log(err))
             },
-            updateAssessment: function(assessment) {
-                if (assessment.scaling_step) {
-                    this.cas.scaling_step = assessment.scaling_step
-                }
-                if (assessment.effort_step) {
-                    this.cas.effort_step = assessment.effort_step
-                }
-                if (assessment.history_change_reason) {
-                    this.cas.history_change_reason = assessment.history_change_reason
-                }
-                this.disableButton = false
-            },
-            saveAssessment: function() {
-                axios.patch(`/cases/${ this.cas_id}/`, {
-                    scaling_step: this.cas.scaling_step,
-                    effort_step: this.cas.effort_step,
-                    history_change_reason: this.cas.history_change_reason
-                })
-                .then(res => {
-                    this.update()
-                    this.disableButton = true
-                })
-                .catch(err => console.log(err))
-            },
-            cancel: function() {
-                this.$emit('close')
-                this.$router.push(`/case/${ this.$route.params.id }/`)
-            }
         },
         created: function() {
             this.update()
@@ -114,7 +70,7 @@
 
 <style>
 
-    .assessment {
+    .assessment_history {
         margin: 1rem;
     }
 
