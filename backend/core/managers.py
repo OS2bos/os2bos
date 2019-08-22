f83d566b4d6afe0471e933c58af5c97e2fb7f8b4
Søren Howe Gersager <thesyre@pm.me>
2019-08-22 14:32:09 +0200

    comment

--- backend/core/managers.py
+++ backend/core/managers.py
@@ -64,7 +64,8 @@ class PaymentQuerySet(models.QuerySet):
         STATUS_GRANTED with no EXPECTED modified_by
         STATUS_EXPECTED with no modifies
         STATUS_GRANTED with GRANTED modified_by
-        STATUS_GRANTED with EXPECTED modified_by overruled with start_date, end_date both inclusive
+        STATUS_GRANTED with EXPECTED modified_by
+         overruled with start_date, end_date both inclusive
         """
         from core.models import STATUS_EXPECTED, STATUS_GRANTED
 
