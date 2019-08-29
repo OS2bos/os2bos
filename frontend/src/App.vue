ce31af4da6ef3908227169a8ea46ac219d082cd0
Novik <ns@magenta.dk>
2019-08-29 13:31:20 +0200

    Added styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -135,4 +135,67 @@
         flex: 1;
     }
 
+    /* modal box */
+    .modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .5);
+        display: table;
+        transition: opacity .3s ease;
+    }
+
+    .modal-wrapper {
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .modal-container {
+        max-width: 40rem;
+        margin: 0px auto;
+        padding: 3rem 3.5rem;
+        background-color: var(--grey0);
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+        transition: all .3s ease;
+    }
+
+    .modal-body {
+        margin: 0.5rem 0;
+    }
+
+    .modal-confirm-btn {
+        float: right;
+    }
+
+    .modal-cancel-btn {
+        float: right;
+        margin-left: 0.5rem;
+        background-color: transparent;
+        color: var(--primary);
+        border-color: transparent;
+    }
+
+    .modal-enter {
+        opacity: 0;
+    }
+
+    .modal-leave-active {
+        opacity: 0;
+    }
+
+    .modal-enter .modal-container,
+    .modal-leave-active .modal-container {
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+    }
+
+    .modal-form {
+        background-color: none;
+        padding: 0;
+    }
+
 </style>
