c3bd3e7a077f5ec97cc0882c178148a6b2ec7641
Søren Howe Gersager <thesyre@pm.me>
2019-09-02 13:21:24 +0200

    add check for payment_frequency in ActivitySerializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -160,7 +160,8 @@ class ActivitySerializer(WritableNestedModelSerializer):
 
         # monthly payments should start and end at the 1st.
         is_monthly_payment = (
-            data["payment_plan"]["payment_frequency"]
+            "payment_frequency" in data["payment_plan"]
+            and data["payment_plan"]["payment_frequency"]
             == PaymentSchedule.MONTHLY
         )
         if is_monthly_payment and (
