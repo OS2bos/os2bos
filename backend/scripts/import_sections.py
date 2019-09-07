0a0f71a64003dcd52806a2c98af8ede9bd958ec7
Carsten Agger <carstena@magenta.dk>
2019-09-07 12:58:38 +0000

    Revert "Remove KLE number and SBSYS template ID from import of Sections."
    This reverts commit b685bcb8f56ba21bd2b9597a7d908114e9af1024.

--- backend/scripts/import_sections.py
+++ backend/scripts/import_sections.py
@@ -22,6 +22,8 @@ with open("paragraffer.csv") as csvfile:
     for row in rows[1:]:
         key = row[3]
         text = row[4]
+        kle = row[6]
+        sbsys_template_id = row[8]
 
         action_tracks = [x.strip() for x in row[11].split(",") if x != ""]
         target_groups = [x.strip() for x in row[12].split(",") if x != ""]
@@ -59,6 +61,8 @@ with open("paragraffer.csv") as csvfile:
         Section.objects.update_or_create(
             **{
                 "paragraph": key,
+                "kle_number": kle,
+                "sbsys_template_id": sbsys_template_id,
                 "text": text,
                 "law_text_name": law_text_name,
                 "allowed_for_disability_target_group": "Handicap"
