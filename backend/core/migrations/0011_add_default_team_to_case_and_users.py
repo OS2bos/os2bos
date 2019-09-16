d92c12b6479c7d34859f86d4b7fb7b545f58d626
Søren Howe Gersager <thesyre@pm.me>
2019-09-16 14:34:28 +0200

    set leader as admin user

--- backend/core/migrations/0011_add_default_team_to_case_and_users.py
+++ backend/core/migrations/0011_add_default_team_to_case_and_users.py
@@ -4,8 +4,12 @@ from django.db import migrations
 def add_default_team_for_case(apps, schema_editor):
     Team = apps.get_model("core", "Team")
     Case = apps.get_model("core", "Case")
+    User = apps.get_model("core", "User")
 
-    team, unused = Team.objects.get_or_create(name="S-DIG")
+    admin, created = User.objects.get_or_create(username="admin")
+    team, created = Team.objects.get_or_create(
+        name="S-DIG", defaults={"leader": admin}
+    )
     Case.objects.filter(team__isnull=True).update(team=team)
 
 
@@ -13,7 +17,10 @@ def add_default_team_for_user(apps, schema_editor):
     Team = apps.get_model("core", "Team")
     User = apps.get_model("core", "User")
 
-    team, unused = Team.objects.get_or_create(name="S-DIG")
+    admin, created = User.objects.get_or_create(username="admin")
+    team, created = Team.objects.get_or_create(
+        name="S-DIG", defaults={"leader": admin}
+    )
     User.objects.filter(team__isnull=True).update(team=team)
 
 
