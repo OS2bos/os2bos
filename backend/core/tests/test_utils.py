80df1311486752319940b5efa6cd03d2bd26dc49
Søren Howe Gersager <thesyre@pm.me>
2019-10-16 13:21:21 +0200

    add test case for no included_list parameter to send_appropriation

--- backend/core/tests/test_utils.py
+++ backend/core/tests/test_utils.py
@@ -187,8 +187,58 @@ class SendAppropriationTestCase(TestCase, BasicTestMixin):
         # Retrieve the mocked template.render call.
         render_call_args = get_template_mock.return_value.render.call_args[1]
         # Assert the activities was part of the call to render.
-        self.assertIn(activity, render_call_args["context"]["main_activities"])
-        self.assertIn(
-            one_time_activity,
+        self.assertCountEqual(
+            [activity], render_call_args["context"]["main_activities"]
+        )
+        self.assertCountEqual(
+            [one_time_activity],
             render_call_args["context"]["supplementary_activities"],
+        )
+
+    @mock.patch("core.utils.HTML")
+    @mock.patch("core.utils.EmailMessage")
+    @mock.patch("core.utils.get_template")
+    def test_send_appropriation_no_included(
+        self, get_template_mock, html_mock, message_mock
+    ):
+        case = create_case(
+            self.case_worker, self.team, self.municipality, self.district
+        )
+        section = create_section()
+        appropriation = create_appropriation(
+            sbsys_id="XXX-YYY", case=case, section=section
+        )
+
+        now = timezone.now().date()
+        activity = create_activity(
+            case,
+            appropriation,
+            start_date=now - timedelta(days=5),
+            end_date=now + timedelta(days=5),
+            activity_type=MAIN_ACTIVITY,
+            status=STATUS_GRANTED,
+        )
+        section.main_activities.add(activity.details)
+        payment_schedule = create_payment_schedule(
+            payment_type=PaymentSchedule.ONE_TIME_PAYMENT
+        )
+        create_activity(
+            case,
+            appropriation,
+            start_date=now - timedelta(days=5),
+            end_date=now - timedelta(days=5),
+            activity_type=SUPPL_ACTIVITY,
+            payment_plan=payment_schedule,
+            status=STATUS_GRANTED,
+        )
+        send_appropriation(appropriation)
+        # Retrieve the mocked template.render call.
+        render_call_args = get_template_mock.return_value.render.call_args[1]
+        # Assert the activities was part of the call to render.
+
+        self.assertCountEqual(
+            [activity], render_call_args["context"]["main_activities"]
+        )
+        self.assertCountEqual(
+            [], render_call_args["context"]["supplementary_activities"]
         )
