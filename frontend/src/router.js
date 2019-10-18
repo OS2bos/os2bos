feb983563db5a3c0a521a0d0387d463987e4c2f6
Carsten Agger <carstena@magenta.dk>
2019-10-18 13:01:42 +0000

    Fix merge error in JS.

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -79,14 +79,6 @@ const router = new Router({
             props: { mode: 'create' }
         },
         {
-            path: '/paymentschedule/',
-            name: 'paymentschedule',
-            component: () => import(/* webpackChunkName: "paymentschedule" */ './components/payment/PaymentSchedule.vue')
-            path: '/login',
-            name: 'login',
-            component: () => import(/* webpackPreload: true */ './components/auth/Login.vue')
-        },
-        {
             // 404 page. This route must declared last
             path: '*',
             name: 'page404',
