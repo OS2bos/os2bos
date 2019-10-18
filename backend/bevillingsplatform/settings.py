ff848bb80e8627a470cbc0e7fae3fd28b76b12f8
Carsten Agger <carstena@magenta.dk>
2019-10-18 07:07:23 +0000

    Update with comments from review.

--- backend/bevillingsplatform/settings.py
+++ backend/bevillingsplatform/settings.py
@@ -233,7 +233,6 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        # "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
 }
