93fb4fb4f2f55487d44be554840b39fb5030de95
Søren Howe Gersager <thesyre@pm.me>
2019-10-21 15:47:14 +0200

    change comment

--- backend/core/migrations/0007_populate_new_payment_id.py
+++ backend/core/migrations/0007_populate_new_payment_id.py
@@ -6,8 +6,8 @@ def populate_payment_id(apps, schema_editor):
     PaymentSchedule = apps.get_model("core", "PaymentSchedule")
     # Initially update all models payment_id with their id.
     PaymentSchedule.objects.update(payment_id=F("id"))
-    # Then start from the top level expected ones and update the lower
-    # with the first payment_id.
+    # Then start from the ones which has a modified_by but no modifies
+    # and go through each modified_by object this way.
     modified_by_payment_schedules = PaymentSchedule.objects.filter(
         activity__modified_by__isnull=False, activity__modifies__isnull=True
     )
