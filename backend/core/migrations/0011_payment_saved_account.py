2d03d090c998f4a05733a29dc2d9ccf5f9f0cc51
Søren Howe Gersager <thesyre@pm.me>
2019-10-24 10:17:13 +0200

    rearrange migrations

--- backend/core/migrations/0011_payment_saved_account.py
+++ backend/core/migrations/0011_payment_saved_account.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("core", "0009_paymentschedule_fictive")]
+    dependencies = [("core", "0010_payment_note")]
 
     operations = [
         migrations.AddField(
