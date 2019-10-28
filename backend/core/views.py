83fea0d68fcd125a4fc521dba96e26d9d8d7c9d2
Søren Howe Gersager <thesyre@pm.me>
2019-10-28 16:29:30 +0100

    upgrade to rest_framework_filters

--- backend/core/views.py
+++ backend/core/views.py
@@ -14,7 +14,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 
-from django_filters import rest_framework as filters
+from rest_framework_filters import rest_framework as filters
 
 from core.models import (
     Case,
