5b211f2b7dd37960d330f227ad4f6f0b9b47a11a
Søren Howe Gersager <thesyre@pm.me>
2019-11-07 14:09:24 +0100

    remove unused Decimal

--- backend/core/tests/test_views.py
+++ backend/core/tests/test_views.py
@@ -5,7 +5,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from decimal import Decimal
 from unittest import mock
 from datetime import date, timedelta
 
