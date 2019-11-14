427ca7e2e4ea7757cbbc4036a99bbd7820cb6f4b
Novik <ns@magenta.dk>
2019-11-14 11:44:41 +0100

    change effort_steps to new api

--- frontend/src/components/assessments/AssessmentEdit.vue
+++ frontend/src/components/assessments/AssessmentEdit.vue
@@ -16,12 +16,9 @@
             <legend>Vurdering</legend>
             <label class="required" for="field-indsatstrappe">Indsatstrappen</label>
             <select id="field-indsatstrappe" v-model="cas.effort_step" @change="updateEffort()" required>
-                <option value="1">Trin 1 - Tidlig indsats i almenområdet</option>
-                <option value="2">Trin 2 - Forebyggelse</option>
-                <option value="3">Trin 3 - Hjemmebaserede indsatser</option>
-                <option value="4">Trin 4 - Anbringelse i slægt eller netværk</option>
-                <option value="5">Trin 5 - Anbringelse i forskellige typer af plejefamilier</option>
-                <option value="6">Trin 6 - Anbringelse i institutionstilbud</option>
+                <option v-for="e in effort_steps" :value="e.id" :key="e.id">
+                    {{ e.name }}
+                </option>
             </select>
             <error err-key="effort_step" />
         
@@ -68,10 +65,18 @@
                     scaling_step: null,
                     assessment_comment: null
                 },
-                create_mode: false
+                create_mode: false,
+                effort_steps: null
             }
         },
         methods: {
+            fetchEffort: function() {
+                axios.get(`/effort_steps/`)
+                .then(res => {
+                    this.effort_steps = res.data
+                })
+                .catch(err => console.log(err))
+            },
             updateEffort: function() {
                 this.$emit('assessment', {
                     effort_step: this.cas.effort_step
@@ -89,6 +94,7 @@
             }
         },
         created: function() {
+            this.fetchEffort()
             if (this.caseObj.id) {
                 this.cas = this.caseObj
             } else {
