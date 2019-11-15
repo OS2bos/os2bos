aad99ce63ccc05309201e9d1216a963f4f9b98ed
iamfrank <frank@magenta.dk>
2019-11-15 08:31:32 +0100

    commit thi

--- frontend-tests/tests/test_visual_regression.js
+++ frontend-tests/tests/test_visual_regression.js
@@ -3,11 +3,11 @@ import { login } from '../utils/logins.js'
 
 
 fixture `Check for visual regression`
-.page('http://localhost:8080/#/')
+.page('http://localhost:8080/')
 
 test('Homepage', async t => {
 
     await login(t)
-    await vrt('check_for_visual_regression', 'homepage', 'url: http://localhost:8080/#/')
+    await vrt('check_for_visual_regression', 'homepage', 'url: http://localhost:8080/')
 
 })
