26e86b6a693cb530c44120f01de66e3935c9349e
Novik <ns@magenta.dk>
2020-04-28 10:29:19 +0200

    change allowed_for_target_groups

--- frontend/src/components/appropriations/AppropriationEdit.vue
+++ frontend/src/components/appropriations/AppropriationEdit.vue
@@ -20,7 +20,7 @@
                 <input id="field-sbsysid" type="text" v-model="appr.sbsys_id" @input="checkKLE(appr.sbsys_id)" required :disabled="appr.granted_from_date">
                 <p class="danger" v-if="sbsysCheck">Sagsnummeret svarer ikke til en af de paragraffer, der kan vælges.</p>
                 <error err-key="sbsys_id" />
-{{cas_target}}
+
                 <label class="required" for="field-lawref">Bevilling efter §</label>
                 <p v-if="preselectedPara"><strong>{{ sections[0].paragraph }} {{ sections[0].text }}</strong></p>
                 <select v-if="!preselectedPara" id="field-lawref" class="listpicker" v-model="appr.section" required :disabled="appr.granted_from_date">
@@ -69,9 +69,9 @@
             },
             cas_target: function() {
                 if (this.cas.target_group === 1) {
-                    return 'allowed_for_family_target_group=true'
+                    return `allowed_for_target_groups=${ this.cas.target_group}`
                 } else if (this.cas.target_group === 2) {
-                    return 'allowed_for_disability_target_group=true'
+                    return `allowed_for_target_groups=${ this.cas.target_group}`
                 } else {
                     return ''
                 }
