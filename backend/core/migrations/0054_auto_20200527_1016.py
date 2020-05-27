3f8301f40ff193f93bc86c8b049ac0652649be96
Søren Howe Gersager <syrelyre@gmail.com>
2020-05-27 11:02:07 +0200

    fix migration sequence

--- backend/core/migrations/0054_auto_20200527_1016.py
+++ backend/core/migrations/0054_auto_20200527_1016.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0052_auto_20200526_1027'),
+        ('core', '0053_auto_20200520_1333'),
     ]
 
     operations = [
