c757128278fc6bfd871832411e982ef99f46dedb
Søren Howe Gersager <syrelyre@gmail.com>
2020-05-27 18:23:40 +0200

    add scaling_step and effort_step to all targetgroups

---
+++ backend/core/migrations/0055_target_group_set_required_fields.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+
+
+def set_targetgroup_required_fields(apps, schema_editor):
+    TargetGroup = apps.get_model("core", "TargetGroup")
+    all_target_groups = TargetGroup.objects.all()
+
+    for target_group in all_target_groups:
+        required_fields = target_group.required_fields_for_case
+        if "scaling_step" not in required_fields:
+            required_fields.append("scaling_step")
+        if "effort_step" not in required_fields:
+            required_fields.append("effort_step")
+        target_group.required_fields_for_case = required_fields
+        target_group.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("core", "0054_auto_20200527_1016")]
+
+    operations = [migrations.RunPython(set_targetgroup_required_fields)]
