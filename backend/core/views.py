5dd95ea4978d53351276fc5dd42c951f5fc01999
Søren Howe Gersager <syrelyre@gmail.com>
2020-05-27 18:41:20 +0200

    remove filter for now

--- backend/core/views.py
+++ backend/core/views.py
@@ -411,7 +411,6 @@ class TargetGroupViewSet(ClassificationViewSetMixin, ReadOnlyViewset):
 
     queryset = TargetGroup.objects.all()
     serializer_class = TargetGroupSerializer
-    filterset_fields = "__all__"
 
 
 class EffortViewSet(ClassificationViewSetMixin, ReadOnlyViewset):
