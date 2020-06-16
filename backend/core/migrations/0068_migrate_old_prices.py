9ca9ac68d8bf58f46be4b610b9206d057a973ab1
Søren Howe Gersager <thesyre@pm.me>
2020-06-16 13:26:45 +0200

    add initial migration for migrating old prices

---
+++ backend/core/migrations/0068_migrate_old_prices.py
@@ -0,0 +1,33 @@
+from django.db import migrations
+
+
+def migrate_old_prices(apps, schema_editor):
+    PaymentSchedule = apps.get_model("core", "PaymentSchedule")
+    Price = apps.get_model("core", "Price")
+
+    for payment_schedule in PaymentSchedule.objects.all():
+        if (
+            payment_schedule.payment_type == PaymentSchedule.ONE_TIME_PAYMENT
+            or payment_schedule.payment_type == PaymentSchedule.RUNNING_PAYMENT
+        ):
+            # These payment types are already handled by setting
+            # a default payment_cost_type of FIXED_PRICE
+            continue
+
+        elif (
+            payment_schedule.payment_type == PaymentSchedule.PER_HOUR_PAYMENT
+            or payment_schedule.payment_type == PaymentSchedule.PER_DAY_PAYMENT
+            or payment_schedule.payment_type == PaymentSchedule.PER_KM_PAYMENT
+        ):
+            # These payment types should have a Price with a single period.
+            price = Price.objects.create(payment_schedule=payment_schedule)
+            price.set_rate_amount(
+                payment_schedule.payment_amount, start_date=None, end_date=None
+            )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("core", "0067_auto_20200615_1846")]
+
+    operations = [migrations.RunPython(migrate_old_prices)]
