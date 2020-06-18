daa50bceb7e24befd2f40a5e68631d97aa3a9b38
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-06-18 09:19:07 +0200

    fixed linting

--- backend/core/management/commands/initialize_database.py
+++ backend/core/management/commands/initialize_database.py
@@ -9,6 +9,7 @@ from django.core.management.base import BaseCommand
 from bevillingsplatform.initialize import initialize
 from django.conf import settings
 
+
 class Command(BaseCommand):
     """
     Initialize database.
