7da7ed495eb559575ba4aef0c5c08a4a6fbb9453
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-06-18 13:13:58 +0200

    use if not instead of if bla is not True

--- backend/core/management/commands/initialize_database.py
+++ backend/core/management/commands/initialize_database.py
@@ -28,7 +28,7 @@ class Command(BaseCommand):
     help = "Call initialize function to seed the database"
 
     def handle(self, *args, **options):
-        if settings.INITIALIZE_DATABASE is not True:
+        if not settings.INITIALIZE_DATABASE:
             return
 
         # Display action
