c802a3b873835d4363ea05cc18ec52fdeeeb1ee6
Søren Howe Gersager <syrelyre@gmail.com>
2020-06-23 10:28:51 +0200

    improvements to cronjob

--- backend/core/management/commands/generate_payment_date_exclusions.py
+++ backend/core/management/commands/generate_payment_date_exclusions.py
@@ -5,12 +5,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
-import os
 import sys
 import logging
-
-from datetime import datetime
 
 from django.core.management.base import BaseCommand
 
@@ -20,7 +16,7 @@ from core.utils import generate_payment_date_exclusion_dates
 
 
 logger = logging.getLogger(
-    "bevillingsplatform.generate_payment_date_exclusion_dates"
+    "bevillingsplatform.generate_payment_date_exclusions"
 )
 
 
@@ -32,28 +28,21 @@ class Command(BaseCommand):
             "-y",
             "--years",
             default=None,
-            help=("Generate payment exclusion dates for years"),
+            type=int,
+            nargs="*",
+            help=("years as YYYY YYYY"),
         )
 
     def handle(self, *args, **options):
         """Parse comma-separated years and generate dates."""
         years = options["years"]
-        if years is not None:
-            try:
-                years = years.split(",")
-            except ValueError:
-                print("Please enter years as comma-separated 'YYYY,YYYY'.")
-                logger.error(
-                    f"Invalid years input {years} - should parse as 'YYYY,YYYY'"
-                )
-                sys.exit(1)
         try:
             dates = generate_payment_date_exclusion_dates(years)
             for date in dates:
                 PaymentDateExclusion.objects.get_or_create(date=date)
 
             logger.info(
-                f"Success: Payment Exclusion Dates were generated for {years}"
+                f"Success: {len(dates)} Payment Exclusion Dates were generated for {years}"
             )
         except Exception:
             logger.exception(
