437fefa4ddb479bb992b7799a947585e97f53992
Carsten Agger <carstena@magenta.dk>
2020-09-15 14:03:23 +0000

    [38231] Change output CSV file according to Phase 3 model changes.

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -718,6 +718,23 @@ def generate_payments_report_list(payments):
             effort_step = case.effort_step
             scaling_step = case.scaling_step
 
+        if (
+            payment_schedule.payment_cost_type
+            == payment_schedule.PER_UNIT_PRICE
+        ):
+            price_per_unit = payment_schedule.price_per_unit.get_rate_amount(
+                payment.date
+            )
+        elif (
+            payment_schedule.payment_cost_type
+            == payment_schedule.GLOBAL_RATE_PRICE
+        ):
+            price_per_unit = payment_schedule.payment_rate.get_rate_amount(
+                payment.date
+            )
+        else:
+            price_per_unit = ""
+
         payment_dict = {
             # payment specific.
             "id": payment.pk,
@@ -737,6 +754,9 @@ def generate_payments_report_list(payments):
             "recipient_id": payment_schedule.recipient_id,
             "recipient_name": payment_schedule.recipient_name,
             "payment_method": payment_schedule.payment_method,
+            "payment_cost_type": payment_schedule.payment_cost_type,
+            "price_per_unit": price_per_unit,
+            "units": payment_schedule.payment_units,
             # activity specific.
             "activity__details__activity_id": activity.details.activity_id,
             "activity__details__name": activity.details.name,
@@ -756,6 +776,7 @@ def generate_payments_report_list(payments):
             "case_worker": str(case.case_worker),
             "team": str(case.team) if case.team else None,
             "leader": str(case.team.leader) if case.team else None,
+            "efforts": ",".join([e.name for e in case.efforts.all()]),
             "effort_step": str(effort_step),
             "scaling_step": str(scaling_step),
             "paying_municipality": str(case.paying_municipality),
