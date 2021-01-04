1b33c7f0d2b0942573bb93917d1eb1f59292dfef
iamfrank <frank@magenta.dk>
2021-01-04 13:06:53 +0100

    reset by reload on payments page

--- frontend/src/components/payments/PaymentSearch.vue
+++ frontend/src/components/payments/PaymentSearch.vue
@@ -213,10 +213,8 @@
         },
         methods: {
             resetValues: function() {
-                // Use the store action to reset values
-                this.$store.dispatch('resetPaymentSearchFilters', 'date-range')
-                this.$store.commit('setPaymentSearchFilter', {})
-                location.reload()
+                // Navigate to page with no url params to reset
+                location = '/payments'
             },
             commitValue: function(key, val) {
                 // Handy helper method that both updates the value in store, 
