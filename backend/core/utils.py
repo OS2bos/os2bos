c10860ec56bd79b0e46897b81f3c3e9efec3aa95
syre <thesyre@pm.me>
2021-03-18 10:39:50 +0100

    add v2 of generate_payments_report_list

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -803,6 +803,14 @@ def generate_payments_report_list_v1(payments):
     """Generate payments report list v1 (v0 with new_account_alias changes)."""
     return generate_payments_report_list_v0(payments, new_account_alias=True)
 
+def generate_payments_report_list_v2(payments):
+    """Generate payments report list v2 (v1 with note added)."""
+    payments_report_list = generate_payments_report_list_v1(payments)
+    notes = models.Payment.objects.values("id", "note")
+
+    for entry in payments_report_list:
+        entry["note"] = notes.get(id=entry["id"])["note"]
+    return payments_report_list
 
 @transaction.atomic
 def write_prism_file_v0(
@@ -969,6 +977,7 @@ def generate_payments_report():
 generate_payments_report_list_versions = {
     "0": generate_payments_report_list_v0,
     "1": generate_payments_report_list_v1,
+    "2": generate_payments_report_list_v2,
 }
 
 write_prism_file_versions = {"0": write_prism_file_v0}
