a1788b2d6671c42fd641a94eaea3e0df7220d85f
iamfrank <frank@magenta.dk>
2021-06-23 16:38:19 +0200

    initial frontend to search and select service provider by CVR search

--- frontend/src/components/payments/edittypes/CVR_select.vue
+++ frontend/src/components/payments/edittypes/CVR_select.vue
@@ -5,19 +5,13 @@
                 <label for="cvr-search-input">Find CVR/P-nr.</label>
                 <input type="search" v-model="search_input" @input="search" id="cvr-search-input">
                 <ul class="cvr-search-result" v-if="search_results.length > 0">
-                    <li v-for="s in search_results" :key="s.id">
+                    <li v-for="s in search_results" :key="s.cvr_number">
                         <button class="cvr-select-btn" @click="select_item(s)" type="button">
                             <dl>
                                 <dt>CVR/P-nr</dt>
-                                <dd>{{ s.id }}</dd>
+                                <dd>{{ s.cvr_number }}</dd>
                                 <dt>Navn</dt>
                                 <dd>{{ s.name }}</dd>
-                                <dt>Adresse</dt>
-                                <dd>{{ s.address }}</dd>
-                                <dt>Branche</dt>
-                                <dd>{{ s.business_sector }}</dd>
-                                <dt>Virksomhedsstatus</dt>
-                                <dd>{{ s.status }}</dd>
                             </dl>
                         </button>
                     </li>
@@ -27,18 +21,20 @@
                 <a href="https://datacvr.virk.dk/data/?language=da" target="_blank">Søg på Virk.dk</a>
             </p>
         </div>
-        <div v-if="selected && selected.id !== 0">
+        <div v-if="service_provider">
             <dl>
-                <dt>CVR/P-nr</dt>
-                <dd>{{ selected.id }}</dd>
+                
                 <dt>Navn</dt>
-                <dd>{{ selected.name }}</dd>
+                <dd><strong>{{ service_provider.name }}</strong></dd>
+                <dt>CVR/P-nr</dt>
+                <dd>{{ service_provider.cvr_number }}</dd>
                 <dt>Adresse</dt>
-                <dd>{{ selected.address }}</dd>
-                <dt>Branche</dt>
-                <dd>{{ selected.business_sector }}</dd>
+                <dd>
+                    {{ service_provider.street }} {{ service_provider.street_number }}<br> 
+                    {{ service_provider.zip_code }}
+                </dd>
                 <dt>Virksomhedsstatus</dt>
-                <dd>{{ selected.status }}</dd>
+                <dd>{{ service_provider.status }}</dd>
             </dl>
         </div>
         <p v-else>
@@ -48,38 +44,49 @@
 </template>
 
 <script>
+import axios from '../../http/Http.js'
+import Timeout from '../../mixins/Timeout.js'
+
 export default {
+    mixins: [
+        Timeout
+    ],
     data: function() {
         return {
             search_results: [],
             search_input: null,
-            selected: null,
-            fake_data: [
-                {
-                    id: 83987673,
-                    name: 'Fiktivt firma',
-                    address: 'Fiktivvej 1234, 1000 Fiktivby',
-                    business_sector: 'Konsulentbistand',
-                    status: 'Aktiv'
-                    
-                },
-                {
-                    id: 87665222,
-                    name: 'Et andet firma',
-                    address: 'Something Something 1234, 2000 Xåby',
-                    business_sector: 'Jordbrug',
-                    status: 'Aktiv'
-                }
-            ]
+            service_provider: null
         }
     },
-    computed: {
-        
+    watch: {
+        service_provider: function(new_sp, old_sp) {
+            if (new_sp !== old_sp) {
+                this.$store.commit('setPaymentPlanProperty', { 
+                    prop: 'recipient_name',
+                    val: new_sp.name
+                })
+                this.$store.commit('setPaymentPlanProperty', { 
+                    prop: 'recipient_id',
+                    val: new_sp.cvr_number
+                })
+                this.$store.commit('setPaymentPlanProperty', { 
+                    prop: 'payment_method',
+                    val: 'INVOICE'
+                })
+            }
+        }
     },
     methods: {
+        fetchData: function(query) {
+            axios.get(`service_providers/fetch_serviceproviders_from_virk/?search_term=${ query }`)
+            .then(res => {
+                console.log(res.data)
+                this.search_results = res.data
+            })
+        },
         search: function() {
             if (this.search_input !== '') {
-                this.search_results = this.fake_data
+                this.fetchData(this.search_input)
             } else {
                 this.search_results = []
             }
@@ -87,8 +94,13 @@ export default {
         select_item: function(item) {
             this.search_input = ''
             this.search_results = []
-            this.selected = item
+            this.service_provider = item
         }
+    },
+    created: function() {
+        // Set debounce on methods that are likely to be fired too often
+        // (ie. while a user is typing into an input field)
+        this.search = this.debounce(this.search, 400)
     }
 }
 </script>
