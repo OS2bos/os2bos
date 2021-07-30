23a703a9a8311381edb5f50ed46fc0fc241147b2
iamfrank <frank@magenta.dk>
2021-07-30 10:24:28 +0200

    change CVR select UI

--- frontend/src/components/payments/edittypes/CVR_select.vue
+++ frontend/src/components/payments/edittypes/CVR_select.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="cvr-search">
-        <div v-if="editable" style="display: flex; justify-content: space-between; align-items: center;">
+        <div v-if="editable && input_visible" class="cvr-search-widget">
             <fieldset>
                 <label for="cvr-search-input">Find virksomhed</label>
                 <input type="search" v-model="search_input" @input="search" id="cvr-search-input" placeholder="CVR/P-nr eller navn">
@@ -8,8 +8,8 @@
                     <li v-for="s in search_results" :key="s.cvr_number">
                         <button class="cvr-select-btn" @click="select_item(s)" type="button">
                             <p class="title"><strong>{{ s.name }}</strong></p>
-                            <p>{{ s.business_code_text }}</p>
-                            <dl>
+                            <p v-if="s.business_code_text">{{ s.business_code_text }}</p>
+                            <dl v-if="s.zip_code">
                                 <dt>CVR/P-nr</dt>
                                 <dd>{{ s.cvr_number }}</dd>
                                 <dt>Adresse</dt>
@@ -26,23 +26,28 @@
         <div v-if="service_provider">
             <dl>
                 <dt>Firmanavn</dt>
-                <dd><strong>{{ service_provider.name }}</strong></dd>
-                <dt>Branchekode</dt>
-                <dd>{{ service_provider.business_code_text }}</dd>
-                <dt>CVR/P-nr</dt>
-                <dd>{{ service_provider.cvr_number }}</dd>
-                <dt>Adresse</dt>
                 <dd>
-                    {{ service_provider.street }} {{ service_provider.street_number }}<br> 
-                    {{ service_provider.zip_code }} {{ service_provider.post_district }}
+                    <strong>{{ service_provider.name }}</strong>
+                    <button type="button" class="link-btn cvr-select-change" v-if="editable && service_provider && !input_visible" @click="input_visible = !input_visible">
+                        <span class="material-icons">edit</span>
+                        Skift
+                    </button>
                 </dd>
-                <dt>Virksomhedsstatus</dt>
-                <dd style="text-transform: lowercase;">{{ service_provider.status }}</dd>
+                <template v-if="service_provider.business_code_text">
+                    <dt>Branchekode</dt>
+                    <dd>{{ service_provider.business_code_text }}</dd>
+                    <dt>CVR/P-nr</dt>
+                    <dd>{{ service_provider.cvr_number }}</dd>
+                    <dt>Adresse</dt>
+                    <dd>
+                        {{ service_provider.street }} {{ service_provider.street_number }}<br> 
+                        {{ service_provider.zip_code }} {{ service_provider.post_district }}
+                    </dd>
+                    <dt>Virksomhedsstatus</dt>
+                    <dd style="text-transform: lowercase;">{{ service_provider.status }}</dd>
+                </template>
             </dl>
         </div>
-        <p v-else>
-            Leverandør ukendt
-        </p>
     </div>
 </template>
 
@@ -62,7 +67,12 @@ export default {
         return {
             search_results: [],
             search_input: null,
-            service_provider: null
+            service_provider: null,
+            input_visible: false,
+            fake_service_provider: {
+                name: 'Ukendt leverandør',
+                cvr_number: '00000000'
+            }
         }
     },
     watch: {
@@ -72,6 +82,14 @@ export default {
     },
     methods: {
         preFetchData: function(recipient_id) {
+            if (!recipient_id) {
+                this.input_visible = true
+                return
+            }
+            if (recipient_id === this.fake_service_provider.cvr_number) {
+                this.service_provider = this.fake_service_provider
+                return
+            }
             axios.get(`service_providers/fetch_serviceproviders_from_virk/?search_term=${ recipient_id }`)
             .then(res => {
                 this.service_provider = res.data.find(sp => {
@@ -82,7 +100,9 @@ export default {
         fetchData: function(query) {
             axios.get(`service_providers/fetch_serviceproviders_from_virk/?search_term=${ query }`)
             .then(res => {
-                this.search_results = res.data
+                let selectables = res.data
+                selectables.unshift(this.fake_service_provider)
+                this.search_results = selectables
             })
         },
         search: function() {
@@ -93,6 +113,8 @@ export default {
             }
         },
         select_item: function(item) {
+            console.log('old reid', this.dataRecipientId)
+            console.log('selecting item', item, this.service_provider)
             this.search_input = ''
             this.search_results = []
             this.service_provider = item
@@ -112,6 +134,9 @@ export default {
                 prop: 'service_provider',
                 val: item
             })
+            this.input_visible = false
+            console.log('selected item', this.service_provider)
+            console.log('new reid', this.dataRecipientId)
         }
     },
     created: function() {
@@ -127,7 +152,16 @@ export default {
 <style>
 
     .cvr-search {
+        
+    }
+
+    .cvr-search-widget {
         position: relative;
+        display: flex; 
+        justify-content: space-between; 
+        align-items: center;
+        border: solid 1px var(--grey2);
+        padding: 0 1rem;
     }
 
     #cvr-search-input {
@@ -141,7 +175,7 @@ export default {
         background-color: white;
         position: absolute;
         top: 3.75rem;
-        left: 0;
+        left: 1rem;
         z-index: 10;
         min-width: 11rem;
         max-height: 40rem;
@@ -186,6 +220,23 @@ export default {
     .cvr-select-btn dl dt {
         margin: 0;
         padding: .5rem 0 0;
+    }
+
+    .cvr-select-change {
+        box-shadow: none;
+    }
+
+    .cvr-select-change:hover,
+    .cvr-select-change:focus {
+        color: var(--grey10);
+        box-shadow: none;
+        text-decoration: underline;
+    }
+
+    .cvr-select-change .material-icons {
+        font-size: 1.5rem;
+        position: relative;
+        bottom: .125rem;
     }
 
 </style>
