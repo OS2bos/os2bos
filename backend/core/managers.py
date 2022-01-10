a046e32e0c1610ce852c947fe122af55da7745b1
syre <thesyre@pm.me>
2022-01-10 20:26:06 +0100

    initial upgrade

--- backend/core/managers.py
+++ backend/core/managers.py
@@ -82,9 +82,11 @@ class PaymentQuerySet(models.QuerySet):
     def amount_sum(self):
         """Sum over Payments with paid_amount overruling amount."""
         return (
-            self.aggregate(amount_sum=Coalesce(Sum(self.amount_case), 0))[
-                "amount_sum"
-            ]
+            self.aggregate(
+                amount_sum=Coalesce(
+                    Sum(self.amount_case), 0, output_field=DecimalField()
+                )
+            )["amount_sum"]
             or 0
         )
 
@@ -454,7 +456,14 @@ class AppropriationQuerySet(models.QuerySet):
             .exclude(sbsys_common=None)
             .values("sbsys_common")
             .annotate(ids=ArrayAgg("id", distinct=True))
-            .annotate(id_count=Func("ids", Value(1), function="array_length"))
+            .annotate(
+                id_count=Func(
+                    "ids",
+                    Value(1),
+                    function="array_length",
+                    output_field=IntegerField(),
+                )
+            )
             .filter(id_count__gt=1)
         )
 
