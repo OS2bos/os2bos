4a22471a5a18a387e1c6c879b5f07857bc520fd5
Frank Thomsen <frank@magenta-aps.dk>
2022-01-19 16:03:44 +0100

    #48036 to/from export dates in GUI

--- frontend/src/components/export/ExportActions.vue
+++ frontend/src/components/export/ExportActions.vue
@@ -22,27 +22,32 @@
             <label for="export-test-checkbox">Testudtræk (registreres ikke)</label>
         </fieldset>
         <fieldset style="margin: 0;">
-            <input 
+            <input
                 type="radio" 
                 value="TARGET1" 
                 v-model="export_target" 
                 id="export-target-radio-1">
             <label for="export-target-radio-1">Forebyggende foranstaltninger</label>
 
-            <input 
+            <input
                 type="radio" 
                 value="TARGET2"
                 v-model="export_target" 
                 id="export-target-radio-2">
             <label for="export-target-radio-2">Handicapkompenserende indsatser</label>
         </fieldset>
-        <fieldset>
-            <label for="export-date">
-                Eksportér ændringer fra denne dato:
-                <br>
-                <span class="dim">(Udelad for at vælge alle)</span>
-            </label>
-            <input id="export-date" type="date" v-model="export_date">
+        <fieldset style="display: flex; flex-flow: row nowrap; gap: .5rem;">
+            <div>
+                <label for="export-from-date">
+                    Fra dato:
+                </label>
+                <input id="export-from-date" type="date" v-model="export_from_date" :max="today"><br>
+                <span class="dim" style="font-size: smaller;">(Udelad for at vælge alle)</span>
+            </div>
+            <div>
+                <label for="export-to-date">Til dato:</label>
+                <input id="export-to-date" type="date" v-model="export_to_date" :max="today" required>
+            </div>
         </fieldset>
         <fieldset>
             <a v-if="export_test" :href="`/api${export_url}`" target="_blank" class="btn dst-export-test-link">
@@ -60,6 +65,7 @@
 
 <script>
 import Warning from '../warnings/Warning.vue'
+import {epoch2DateStr} from '../filters/Date.js'
 
 export default {
     components: {
@@ -69,20 +75,26 @@ export default {
         return {
             export_target: "TARGET1",
             export_test: true,
-            export_date: null
+            export_from_date: null,
+            export_to_date: epoch2DateStr(new Date()),
+            today: epoch2DateStr(new Date())
         }
     },
     computed: {
         export_url: function() {
             let target_str = 'appropriations/generate_dst_preventative_measures_file/'
-            let test_str = 'test=true'
-            const date_str = this.export_date ? `from_date=${this.export_date}` : 'from_date=1970-01-01'
             if (this.export_target === 'TARGET2') {
                 target_str = 'appropriations/generate_dst_handicap_file/'
             }
+
+            let date_str = `to_date=${this.export_to_date}&from_date=`
+            date_str += this.export_from_date ? this.export_from_date : '1970-01-01' // '1970-01-01' will be interpreted as 'from the beginning'
+            
+            let test_str = 'test=true'
             if (!this.export_test) {
                 test_str = 'test=false'
             }
+
             return `/${target_str}?${test_str}&${date_str}`
         }
     },
