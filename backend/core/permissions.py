a9fda4baac821cb61d99f0b642d4b964e2ff49d3
syre <thesyre@pm.me>
2022-02-01 10:21:37 +0100

    modify graphql api permissions to allow post add AuthMiddleware for GraphQL schema add tests, change graphql get to post to mimic frontend

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -6,6 +6,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Custom permissions used by this app."""
 
+from django.contrib.auth import get_user_model
 from rest_framework import permissions
 
 from core.models import (
@@ -17,9 +18,13 @@ from core.models import (
     CASH,
 )
 
+UserModel = get_user_model()
+
 
 class IsUserAllowed(permissions.BasePermission):
-    """Determine user's frontend permissions i.e., in all API operations."""
+    """Base model for determining user's API permissions."""
+
+    SAFE_METHODS = []
 
     def has_permission(self, request, view):
         """Allow depending on operation and user's profile."""
@@ -30,7 +35,7 @@ class IsUserAllowed(permissions.BasePermission):
             # which have no profile and should have no access either.
             return False
         if profile == User.READONLY:
-            return request.method in permissions.SAFE_METHODS
+            return request.method in self.SAFE_METHODS
         elif profile == User.EDIT:
             return view.get_view_name() != "Grant"
         elif profile in [User.GRANT, User.WORKFLOW_ENGINE, User.ADMIN]:
@@ -41,7 +46,41 @@ class IsUserAllowed(permissions.BasePermission):
             return False
 
 
-class NewPaymentPermission(IsUserAllowed):
+class IsUserAllowedREST(IsUserAllowed):
+    """Determine user's permissions in REST API operations."""
+
+    SAFE_METHODS = permissions.SAFE_METHODS
+
+
+class IsUserAllowedGraphQL(IsUserAllowed):
+    """Determine user's permissions in GraphQL API operations."""
+
+    # For GraphQL we also allow POST as a safe method for readonly users.
+    SAFE_METHODS = permissions.SAFE_METHODS + ("POST",)
+
+
+class GraphQLAuthMiddleware(object):
+    """Middleware for restricting users executing mutations."""
+
+    def resolve(self, next, root, info, **args):
+        """If it's a mutation we only allow it for specific profiles."""
+        if info.operation.operation == "mutation":
+            user = info.context.user
+            if user.profile in [
+                UserModel.EDIT,
+                UserModel.GRANT,
+                UserModel.WORKFLOW_ENGINE,
+                UserModel.ADMIN,
+            ]:
+                # This user can do everything in the frontend.
+                return next(root, info, **args)
+            else:
+                # No recognized profile, no access
+                return None
+        return next(root, info, **args)
+
+
+class NewPaymentPermission(IsUserAllowedREST):
     """Check if adding a new payment is allowed."""
 
     def has_permission(self, request, view):
