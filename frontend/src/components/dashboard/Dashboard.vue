9849fa66e476f0bd2fa31d58102499d4d18fb152
iamfrank <frank@magenta.dk>
2022-02-04 21:26:30 +0100

    initial chart experiment done

--- frontend/src/components/dashboard/Dashboard.vue
+++ frontend/src/components/dashboard/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
     <article>
-        <h1>Økonomioverblik</h1>
+        <h1>Udgifter {{year}}</h1>
         <canvas id="chart1" width="400" height="400"></canvas>
         <p></p>
 
@@ -45,7 +45,6 @@ export default {
             ajax.post('/graphql/', data)
             .then(res => {
                 const data = res.data.data
-                console.log('got data', data)
                 this.planned_payments = this.calcMonthlyPayments(data.plannedPayments.edges)
                 this.actual_payments = this.calcMonthlyPayments(data.actualPayments.edges)
                 this.renderChart(this.actual_payments, this.planned_payments)
@@ -53,14 +52,21 @@ export default {
                     
         },
         calcMonthlyPayments: function(payment_list) {
+            console.log('got data', payment_list)
+            const date_key = payment_list[0].node.date ? 'date' : 'paidDate' 
+            const amount_key = payment_list[0].node.amount ? 'amount' : 'paidAmount' 
+            
             let monthly_payments = [0,0,0,0,0,0,0,0,0,0,0,0]
+
             payment_list.forEach(payment => {
-                const month = Number(payment.node.date.substring(5,2))
-                console.log('got a month', month)
+                const month_index = Number(payment.node[date_key].substring(5,7)) - 1
+                const sum = new Number(monthly_payments[month_index])
+                monthly_payments[month_index] = sum + Number(payment.node[amount_key])
             })
+            console.log('got calc', monthly_payments)
             return monthly_payments
         },
-        renderChart: function(paymentdata) {
+        renderChart: function(actual_payments, planned_payments) {
 
             const ctx = document.getElementById('chart1').getContext('2d');
             const myChart = new Chart(ctx, {
@@ -69,18 +75,18 @@ export default {
                     labels: ['jan', 'feb', 'mar', 'apr', 'maj', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'],
                     datasets: [
                         {
-                            label: 'Planlagte udgifter',
-                            data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
+                            label: 'Betalt',
+                            data: actual_payments,
                             backgroundColor: [
-                                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red'
+                                'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
                             ],
                             borderWidth: 1
                         },
                         {
-                            label: 'Betalt',
-                            data: [3,1,4,3,6,3,13,3,9,2,13,8],
+                            label: 'Planlagte udgifter',
+                            data: planned_payments,
                             backgroundColor: [
-                                'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue',
+                                'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red'
                             ],
                             borderWidth: 1
                         }
